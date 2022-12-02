3d058731b5605badf11552632c50250ab742acbb
AliMasud05 <morshedalimasud3@gmail.com>
2022-12-02 20:01:21 +0600

    delet buyer

--- index.js
+++ index.js
@@ -87,6 +87,15 @@ async function run() {
 
       
     });
+    app.delete('/buyers/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: ObjectId(id)
+      }
+      const result = await usersCollection.deleteOne(query);
+      res.send(result)
+
+    })
 
   } finally {
   }
